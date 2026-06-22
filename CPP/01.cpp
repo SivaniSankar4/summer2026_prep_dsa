30b9b8a4159eaa10114148b4f181f77b7ae10f74
SivaniSankar <sivani04022004@gmail.com>
2026-06-22 11:02:23 +0530

    cpp basics

---
+++ CPP/01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){ //main is name of method whose data type is int, it doesnt necessarily return int
+
+//Data types Variables
+    // int age; // in c++ if not initialised then the memory block holds garbage value.
+    // cout<<age<<endl;
+    // char name ='A';
+    // bool ans = true;
+
+    // /*int -> 4 bytes
+    // long long-> 8 bytes
+    // float-> 4 bytes. for shorter ,defined ranges for example:marks
+    // double -> 8 bytes
+
+    // char-> 1 byte. in ASCII it takes 1 , but more in unicode
+    // bool -> 1 byte*/
+
+//Conditional statements
+    int age=20;
+    int num=4;
+    if(age>10&&num>10){
+        cout<<"test conducted"<<endl;
+    }else{
+        cout<<"test not conducted"<<endl;
+    }
+
+
+}
