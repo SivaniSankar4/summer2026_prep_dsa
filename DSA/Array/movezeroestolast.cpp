37c3db7fe02afb347caacd69adaae4a041785ef8
SivaniSankar <sivani04022004@gmail.com>
2026-06-29 23:54:11 +0530

    leetcode 283

---
+++ DSA/Array/movezeroestolast.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i+1<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+            
+        }
+    }
+};
+int main(){
+    vector<int> nums={1,4,0,2,0,5,0,0,5,6};
+    Solution s;
+    s.moveZeroes(nums);
+    return 0;
+}
