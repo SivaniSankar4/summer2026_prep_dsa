2783718317173b4551200be7c8a3b0a9c01c0e0a
SivaniSankar <sivani04022004@gmail.com>
2026-07-06 21:44:41 +0530

    insertion, deletion and printing in a singly linked list.

--- DSA/Linked List/basic.cpp
+++ DSA/Linked List/basic.cpp
@@ -38,16 +38,76 @@ void printLL(Node* head){
     }
     cout<<endl;
 }
-void printeven(Node* head){
+// void printeven(Node* head){
+//     Node* temp=head;
+//     while(temp!=NULL){
+//         if(temp->data%2==0){
+//             cout<<temp->data<<" ";
+//         }
+//         temp=temp->next;
+//     }
+//     cout<<endl;
+// }
+void deleteatend(Node* &head){
+    if(head==NULL){
+        return;
+    }
+    if(head->next==NULL){
+        delete head;
+        head=NULL;
+        return;
+    }
     Node* temp=head;
-    while(temp!=NULL){
-        if(temp->data%2==0){
-            cout<<temp->data<<" ";
-        }
+    while(temp->next->next!=NULL){
         temp=temp->next;
     }
-    cout<<endl;
+    delete temp->next;  //why do we delete in cpp: because if we create something on heap,
+    //that is if we create something using new keyword we have to delete it. we dont delete int because its on stack.
+    temp->next=NULL;
 }
+void deleteatbeginning(Node* &head){
+    if(head==NULL) return;
+    if(head->next==NULL){
+        delete head;
+        return;
+    }
+    Node* temp=head;
+    head=head->next;
+    temp->next=NULL;
+    delete temp;
+}
+void deleteatpos(Node* &head,int position){
+    if(head==NULL) return;
+    Node* temp=head;
+    for(int i=1;i<position-1&&temp!=0;i++){
+        temp=temp->next;
+    }
+    Node* n=temp->next;
+    temp->next=temp->next->next;
+    n->next=NULL;
+    delete n;
+}
+// void deletionofval(Node* &head,int val){
+//     if(head==NULL){
+//         return;
+//     }
+//     Node* temp=head;
+//     if(head->data==val){
+//         head=head->next;
+//         temp->next=NULL;
+//         delete temp;
+//     }
+//     while(temp->next->next!=NULL){
+//         if(temp->next->data==val){
+//             Node* n=temp->next;
+//             temp->next=n->next;
+//             n->next=NULL;
+//             delete n;
+//             break;
+//         }
+//         temp=temp->next
+//     }
+// }
 void insertatposition(Node* &head,int pos,int val){
     Node* node=new Node(val);
     if(pos==1){
@@ -76,9 +136,14 @@ int main(){
    insertAtEnd(head, 3);
    insertAtEnd(head, 4);
    insertAtbeginning(head, 15);
-   printLL(head);
-   printeven(head);
+   //printLL(head);
+   //printeven(head);
    insertatposition(head,4,11);
+   printLL(head);
+   deleteatbeginning(head);
+   deleteatend(head);
+   printLL(head);
+   deleteatpos(head,3);
    printLL(head);
 
 }
