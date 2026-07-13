7fa87eb2b99905ff89a7e0fb253cd9026bd091c6
SivaniSankar <sivani04022004@gmail.com>
2026-07-13 23:16:17 +0530

    https://www.geeksforgeeks.org/problems/insert-an-element-at-the-bottom-of-a-stack/1

---
+++ DSA/StackandQueue/inseratboottom.cpp
@@ -0,0 +1,19 @@
+//https://www.geeksforgeeks.org/problems/insert-an-element-at-the-bottom-of-a-stack/1
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Solution {
+  public:
+    stack<int> insertAtBottom(stack<int> st, int x) {
+        if(st.empty()){
+            st.push(x);
+            return st;
+        }
+        int temp=st.top();
+        st.pop();
+        st=insertAtBottom(st,x);
+        st.push(temp);
+        return st;
+    }
+};
