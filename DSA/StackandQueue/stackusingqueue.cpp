9cda952f92c88997051197c1bb9b8c21ef6cf621
SivaniSankar <sivani04022004@gmail.com>
2026-07-13 23:17:23 +0530

    https://www.geeksforgeeks.org/problems/stack-using-queue/1

---
+++ DSA/StackandQueue/stackusingqueue.cpp
@@ -0,0 +1,39 @@
+//https://www.geeksforgeeks.org/problems/stack-using-queue/1
+#include <iostream>
+#include <queue>
+using namespace std;
+class myStack {
+    queue<int> q;
+
+  public:
+
+    void push(int x) {
+        // Inserts an element x at the top of the stack
+        q.push(x);
+        int n=q.size();
+        for(int i=0;i<n-1;i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    void pop() {
+        // Removes an element from the top of the stack
+        if(q.empty())return;
+        q.pop();
+    }
+
+    int top() {
+        // Returns the top element of the stack
+        // If stack is empty, return -1
+        if(q.empty()){
+           return -1 ; 
+        } 
+        return q.front();
+    }
+
+    int size() {
+        // Returns the current size of the stack
+        return q.size();
+    }
+};
