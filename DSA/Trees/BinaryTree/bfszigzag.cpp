225322572431b5e4b6526184e12812f4d7856376
SivaniSankar <sivani04022004@gmail.com>
2026-07-18 23:34:02 +0530

    level order traversal zig zag

---
+++ DSA/Trees/BinaryTree/bfszigzag.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL)return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        bool l=true;
+        while(!q.empty()){
+            int levelsize=q.size();
+            vector<int> levelelements;
+    
+            for(int i=0;i<levelsize;i++){
+                TreeNode* frontNode=q.front();
+                q.pop();
+                levelelements.push_back(frontNode->val);
+            
+                if(frontNode->left)q.push(frontNode->left);
+                if(frontNode->right)q.push(frontNode->right);
+                
+
+            }
+            if(!l){
+                reverse(levelelements.begin(),levelelements.end());
+            }
+            ans.push_back(levelelements);
+            l=!l;
+        }
+        return ans;
+    }
+};
