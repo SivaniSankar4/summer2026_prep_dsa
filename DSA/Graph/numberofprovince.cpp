c3e77bbd8960b5e6b75d25e74eeeb08bb533eef5
SivaniSankar <sivani04022004@gmail.com>
2026-07-27 23:13:53 +0530

    numberofprovince

---
+++ DSA/Graph/numberofprovince.cpp
@@ -0,0 +1,13 @@
+void dfshelper(){
+    isVisited(city)=true;
+    for(int neighbour=0;neighbour<isConnected.size();neighbour++){
+        if(isConnected[city])
+    }
+}
+for(int i=0;i<n;i++){
+    if(!isvisited[i]){
+        //dfshelper
+        provincecount++;
+    }
+}
+return provincecount;
